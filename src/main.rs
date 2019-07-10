aabef7dbad7202d89fb41bf35bdb39df7cdbefe4
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2019-07-09 22:57:33 -0700

    Initial commit of parser grammar for atoms and numbers.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
-use bfg_prolog::{Machine, Functor};
+#![allow(unused)]
 
+use bfg_prolog::{Machine, Functor};
+use lalrpop_util::lalrpop_mod;
+
+
+lalrpop_mod!(pub parser);
 
 fn main() {
-    let m = Machine::new();
+    let atom_parser = parser::AtomParser::new();
 
-    let f1 = Functor(String::from("foo"), 2);
-    let f2 = f1.clone();
+    // atoms
+    assert!(atom_parser.parse("22").is_err());
+    assert!(atom_parser.parse("_Abc").is_err());
+    assert!(atom_parser.parse("Abc").is_err());
+    assert!(atom_parser.parse("abc").is_ok());
+    assert!(atom_parser.parse("'Abc'").is_ok());
+    assert!(atom_parser.parse("'Abc").is_err());
+    assert!(atom_parser.parse(".q").is_err());
+    assert!(atom_parser.parse("snake_case").is_ok());
+    assert!(atom_parser.parse("'snake_case'").is_ok());
+    assert!(atom_parser.parse("This_Fails").is_err());
+    assert!(atom_parser.parse("'This_Succeeds'").is_ok());
 
-    println!("{0:?}\n{0}\n{1}\n{2:?}", &f1, f2.to_string(), m);
+    let number_parser = parser::NumberParser::new();
+
+    // numbers
+    assert!(number_parser.parse("2").is_ok());
+    assert!(number_parser.parse("42").is_ok());
+    assert!(number_parser.parse("34345354").is_ok());
+//    assert!(number_parser.parse("3.3").is_ok());
+//    assert!(number_parser.parse("3.30").is_ok());
+//    assert!(number_parser.parse("0.3").is_ok());
+    assert!(number_parser.parse("a03").is_err());
+    assert!(number_parser.parse("_21").is_err());
+    assert!(number_parser.parse("2_12").is_err());
+    assert!(number_parser.parse(".3").is_err());
+    assert!(number_parser.parse("2.").is_err());
 }
