c1845aeca12fc069dfe9480d2793df3515ce933a
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2019-09-02 14:17:06 -0700

    Updating the parser.

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,17 @@ use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub parser);
 
-fn main() {}
+fn main() {
+    let clause_parser = parser::ClauseParser::new();
+    let assertion_parser = parser::AssertionParser::new();
+
+    let x = clause_parser.parse("p(U, V).").unwrap();
+
+    let a = assertion_parser
+        .parse("p(U, v) :- a, x(A). p(U, v) :- a, x(A).")
+        .unwrap();
+
+    println!("{:?}", a);
+
+    solve_toplevel(x);
+}
