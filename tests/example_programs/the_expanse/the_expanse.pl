af6ff59fa01ab4fee4dfca43903c8431d45c2f61
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2019-09-02 19:18:54 -0700

    Adding example prolog file for testing.

---
+++ tests/example_programs/the_expanse/the_expanse.pl
@@ -0,0 +1,8 @@
+captain('Rocinante', 'James Holden').
+captain('Canterbury', 'McDowell').
+executive_officer('Rocinante', 'Naomi Nagata').
+executive_officer('Canterbury', vacant).
+pilot('Rocinante', 'Alex Kamal').
+pilot('Canterbury', 'Alexa Kamal').
+mechanic('Canterbury', 'Amos Burton').
+mechanic('Rocinante', 'Amos Burton').
